3967612c7663ca4946d67ec882836df6b14a7487
smuth4 <smuth4@L11107042788.wctc.msft>
2015-09-30 19:21:32 -0500

    Class Work 9/30/2015 1.0

--- src/discountstrategy/Customer.java
+++ src/discountstrategy/Customer.java
@@ -12,11 +12,13 @@ package discountstrategy;
 public class Customer {
     private String custID;
     private String custName;
-    
-//    public Customer(String custID, String custName){
-//        
-//    }
 
+    
+    public Customer(String custID, String custName) {
+        this.custID = custID;
+        this.custName = custName;
+    }
+    
     public String getCustID() {
         return custID;
     }
