45782b171265f5b7c1f14f75086bf233f20ac09a
Scott <Scott@192.168.1.128>
2015-10-05 15:01:41 -0500

    Receipt needs testing but is not throwing errors.

--- src/discountstrategy/Receipt.java
+++ src/discountstrategy/Receipt.java
@@ -20,10 +20,11 @@ public class Receipt {
     private double amountTotal;
     
     //Constructor
-//    public Receipt(DataAccessStrategy das, String custId) {
-//        this.das = das;
-//        this.customer = das.findCustomer(custId);
-//    }
+    public Receipt(DataAccessStrategy das, String custId) {
+        this.setDas(das);
+        this.customer = findCustomer(custId);
+        lineItems = new LineItem[0];
+    }
 
     
     //findCustomer
@@ -31,42 +32,7 @@ public class Receipt {
         return das.findCustomer(custID).getCustName();
     }
     
-    //new line item
-    public void addNewLineItem(String prodId, int qty) {
-        //line item constructor object
-        LineItem prodObj = new LineItem(das.findProduct(prodId),qty);
-        //temporary array
-        LineItem[] temp = new LineItem[lineItems.length + 1];
-        //for loop to copy current array into temp array
-        for (int i = 0; i < lineItems.length; i++) {
-            temp[i] = lineItems[i];
-        }
-        //add new line item
-        temp[temp.length - 1] = prodObj;
-        //copy temp array to lineItems array
-        lineItems = temp;
-        //null temp array
-        temp = null;
-        totalSubtotal += prodObj.getSubtotal();
+    public void setDas(DataAccessStrategy das) {
+        this.das = das;
     }
-    
-    //get the array
-    public LineItem[] getLineItemArray() {
-        return lineItems;
-    }
-    
-//    public static void main(String[] args) {
-//
-//        Receipt receipt = new Receipt();
-//
-//        receipt.addNewLineItem("2002", 4);
-//        receipt.addNewLineItem("2004", 1);
-//
-//        LineItem[] items = receipt.getLineItemArray();
-//        for (int i = 0; i < receipt.getLineItemArray().length; i++) {
-//            System.out.println(items[i].getProduct().getProdId() + "\t"
-//                    + items[i].getProduct().getProdName() + "\t" + items[i].getQty()
-//                    + "\t" + items[i].getSubtotal() + "\t" + items[i].getDiscount());
-//        }
-//}
 }
